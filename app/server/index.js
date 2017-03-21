c26701caf11907c0d993d8333f9bf0d1bac0f9b0
pil-a <pil-a@yandex.ru>
2017-03-21 20:24:17 +0300

    always redirect to index for all pathes

--- app/server/index.js
+++ app/server/index.js
@@ -1,29 +1,26 @@
 var path = require('path');
-
-// Read config
 var nconf = require('nconf');
 nconf.file({ file: path.join(__dirname, 'config.json') });
 
-// Initialize Express
 var express = require('express');
 var app = express();
 
-// Serve static files
 app.use(express.static(path.join(__dirname, '..', 'client')));
 
 // Define routes
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+app.get('*', function(req, res) {
+  res.redirect(path.join(__dirname, 'views', 'index.html'));
+});
 
-// Start server
 var port = nconf.get('general').port;
 var http = require('http');
 var server = http.createServer(app).listen(port, function() {
   console.log('App is started at PORT ' + port);
 });
 
-// Export some stuff for tests
 var cleanup = function() {
   server.close();
 }
